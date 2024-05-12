1a9448cb2534edc0490a1ee3fca262fb2c45e95d
JiMVP <jemsadimalas@gmail.com>
2024-05-12 10:22:22 +0800

    alumni v12.5.24

---
+++ home.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Alumni Information Management System</title>
+	<link rel="stylesheet" href="alum.css">
+	
+</head>
+<body>
+	<div class="std-contain">
+		<div class="topbar">
+			<div >
+				TOPBAR
+			</div>
+		</div>
+		<div class="masthd">MASTHEAD</div>
+		<div class="banner">BANNER</div>
+		<div class="auxmenu">AUXILIARY MENU</div>
+		<div class="content">CONTENT AREA</div>
+		<div class="agenft">AGENCY FOOTER</div>
+		<div class="stdft">STANDARD FOOTER</div>
+
+	</div>
+	
+
+</body>
+</html>
