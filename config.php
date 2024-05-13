10a99c3294ca4d83f7beb794f6c39796d559ac99
JiMVP <jemsadimalas@gmail.com>
2024-05-13 16:43:33 +0800

    win11pc: modified css config conn and login

--- config.php
+++ config.php
@@ -10,11 +10,12 @@
 		$usn=$_POST['login-usn'];
 		$pwd=$_POST['login-pwd'];
 
-		$sql=mysqli_query($con, "SELECT * FROM `alum-user` WHERE `usn` = '$usn';
+		$sql=mysqli_query($con, "SELECT * FROM `alum-user` WHERE `usn` = '$usn'");
 		$sqlfet=mysqli_fetch_assoc($sql);
 		$fusn=$sqlfet['usn'];
+		$fpwd=$sqlfet['pwd'];
 		
-		if($usn==$fusn){
+		if($usn==$fusn && $pwd==$fpwd){
 			header("location:home.php");
 		}else{
 			header("location:login.html");
--- conn.php
+++ conn.php
@@ -7,7 +7,7 @@
 
 	$con = mysqli_connect($host,$host_user,$host_pass,$dbname);
 
-	if(!$conn){
+	if(!$con){
 		echo "Error\nCheck Database Connection";
 	}
 
