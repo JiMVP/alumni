42baae7bec16778130a6c472b3578069a6cb16f2
JiMVP <jemsadimalas@gmail.com>
2024-09-19 23:12:39 +0800

    Alumni v19.9.24

---
+++ homebs5.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="index.css" rel="stylesheet">
+    <link rel="stylesheet" href="alumni.css">
+    
+    
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .idk{
+            color:white;
+            background-color:rgb(1, 3, 126);
+            margin:0px;
+        }
+        
+        .idk2{
+            color:black;
+            background-color:gray;
+            margin:0px;
+        }
+        /* .std-contain{
+            background-color: whitesmoke;
+            width: 1190px;
+            margin:auto;
+            min-height: 600px;
+        } */
+    </style>
+</head>
+<body>
+
+    <div name="home-topbar" class="container-fluid-sm text-center idk">
+        
+        <div name="ht-menu" class="container-sm">
+            <div class="row">
+
+                <div class="col-sm-4 m-0 p-4">
+                    <b>ACLC <?php echo "BRANCH" ?> ALUMNI</b>
+                </div>
+            
+                <a href="" class="col-sm-2 m-0 tb-menu-selectedopt">
+                
+                        
+                    
+                    <!-- <p class="h6"> -->
+                        TEST
+                    <!-- </p> -->
+                    
+                    
+                    
+                </a>
+                <a href="test.php" class="col-sm-2 m-0 p-4 tb-menu-opt">
+                    
+                        
+                    <!-- <p class="h6"> -->
+                        TEST
+                    <!-- </p> -->
+                        
+                    
+                </a>
+            </div>
+        </div>
+        
+    </div>
+    <div class="std-contain">
+    </div>
+</body>
+</html>
